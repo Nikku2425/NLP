601635e899aedf3758e215414a2241b609d6b448
Nikku2425 <158567427+Nikku2425@users.noreply.github.com>
2024-03-29 10:48:54 +0530

    Update and rename WS-2.py to NLP_WS-2.py

--- NLP_WS-2.py
+++ NLP_WS-2.py
@@ -1,7 +1,7 @@
 import spacy
 nlp=spacy.load('en_core_web_sm')
 
-doc=nlp("nikhil is erripuku, Anji is also an erripuku")
+doc=nlp("nikhil is Good , Anji is also an erripuku")
 
 for word in doc:
   print(word," | ",word.pos_)
